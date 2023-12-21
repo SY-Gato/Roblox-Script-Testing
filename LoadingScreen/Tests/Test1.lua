629aa8d9675a13f6efa3aa2fdf3737837abef6b7
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-12-21 09:26:48 -0600

    Update Test1.lua

--- LoadingScreen/Tests/Test1.lua
+++ LoadingScreen/Tests/Test1.lua
@@ -1,4 +1,18 @@
 -- Created by master200crate
 -- Put in ReplicatedFirst
-local SingQuote = s
-if not game:GetService("RunService"):IsClient() then error("Must be object type 'LocalScript'")
+if not game:GetService("RunService"):IsClient() then error("Must be object type LocalScript") end
+if not script.Parent == game.ReplicatedFirst then error("Script must be in ReplicatedFirst") end
+
+local contentProvider = game:GetService("ContentProvider")
+local function CreateUi()
+  local sgui = Instance.new("ScreenGui")
+  local txtlabel = Instance.new("TextLabel")
+  sgui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+  sgui.IgnoreGuiInset = True
+  txtlabel = sgui
+  txtlabel.Name = "InsideText"
+  txtlabel.BackgroundColor3 = Color3.fromRGB(30,30,30)
+  txtlabel.TextColor3 = Color3.fromRGB(255,255,255)
+  txtlabel.Text = "0/0"
+  return sgui
+end
