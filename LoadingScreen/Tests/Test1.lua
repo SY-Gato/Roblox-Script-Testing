94c27e03a7b2d5a08c6b34a3910a69d7def6dd72
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-12-21 10:39:07 -0600

    Update Test1.lua

--- LoadingScreen/Tests/Test1.lua
+++ LoadingScreen/Tests/Test1.lua
@@ -8,11 +8,15 @@ local function CreateUi()
   local txtlabel = Instance.new("TextLabel")
   sgui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
   sgui.IgnoreGuiInset = True
+  sgui.Parent = script
   txtlabel = sgui
   txtlabel.Name = "InsideText"
   txtlabel.BackgroundColor3 = Color3.fromRGB(30,30,30)
   txtlabel.TextColor3 = Color3.fromRGB(255,255,255)
   txtlabel.Text = "0/0"
+  txtlabel.Size = Udim2.new(1,0,1,0)
+  txtlabel.Font = Enum.Font.Ubuntu
+  txtlabel.TextSize = 20
   return sgui
 end
 
@@ -20,3 +24,19 @@ local contentProvider = game:GetService("ContentProvider")
 local ui = CreateUi()
 
 repeat wait() until game:IsLoaded()
+
+local assets = game:GetDescendants()
+local maxAssets = #assets
+
+local plr = game.Players.LocalPlayer
+local plrGui = plr:WaitForChild("PlayerGui")
+
+ui.Parent = plrGui
+
+for i, assetToLoad in assets do
+  contentProvider:PreloadAsync({assetToLoad})
+  ui:WaitForChild("InsideText").Text = i.."/"..maxAssets
+end
+
+wait(1)
+ui:Destroy()
