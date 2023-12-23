a55125c4131ba41b61769a5fe6d59063dd3b7f33
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-12-23 11:20:00 -0600

    Update PrivServerTp.lua

--- PrivateServerTeleport/Tests/PrivServerTp.lua
+++ PrivateServerTeleport/Tests/PrivServerTp.lua
@@ -1,1 +1,17 @@
+local plrslist = {}
+local plrs = game:GetService("Players")
+local ts = game:GetService("TeleportService")
+local id = game.PlaceId
 
+local code = ts:ReserveServer(id)
+plrslist = plrs:GetPlayers()
+local totp = {}
+for i=1,#plrslist do
+  if plrs:FindFirstChild(plrslist[i]) do
+      table.insert(totp,plrs:FindFirstChild(plrslist[i]))
+    else
+      table.remove(plrslist, i)
+    end
+  end
+end
+ts:TeleportToPrivateServer(id, code, totp)
